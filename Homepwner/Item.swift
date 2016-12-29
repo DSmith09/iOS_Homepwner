a6bdeef9402061c3834c73537530acec29c020f7
David <dmsmith@Davids-MacBook-Pro.local>
2016-12-29 11:46:29 -0500

    Initial Commit/Added Item Model/Datastore

---
+++ Homepwner/Item.swift
@@ -0,0 +1,62 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by David on 12/29/16.
+//  Copyright © 2016 DSmith. All rights reserved.
+//
+import Foundation
+
+class Item : NSObject {
+    private let name : String!
+    private let valueInDollars : Float!
+    private let serialNumber : String?
+    private let dateCreated : NSDate!
+    
+    // Default Ctor
+    init(name: String, valueInDollars: Float, serialNumber: String?) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = NSDate()
+        
+        super.init()
+    }
+    
+    // Convenience Ctor
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spoon", "Mac"]
+            
+            let randomAdjective = adjectives[Int(arc4random_uniform(UInt32(adjectives.count)))]
+            let randomNoun = nouns[Int(arc4random_uniform(UInt32(nouns.count)))]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Float(arc4random_uniform(100))
+            let randomSerial = NSUUID.init().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName, valueInDollars: randomValue, serialNumber: randomSerial)
+            
+        } else {
+            self.init(name: "", valueInDollars: 0, serialNumber: nil)
+        }
+    }
+    
+    // Getters
+    open func getName() -> String {
+        return name
+    }
+    
+    open func getValue() -> Float {
+        return valueInDollars
+    }
+    
+    open func getSerialNumber() -> String? {
+        return serialNumber
+    }
+    
+    open func getDateCreated() -> NSDate {
+        return dateCreated
+    }
+}
---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,29 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by David on 12/29/16.
+//  Copyright © 2016 DSmith. All rights reserved.
+//
+
+import UIKit
+
+// Utility Class for Creating/Storing Items
+class ItemStore {
+    private static var allItems = [Item]()
+    
+    // Utility Method to Create New Item
+    open static func createItem() -> Item {
+        return Item(random: true)
+    }
+    
+    // Utility Method to Add New Item to Datastore
+    open static func addItem(item: Item) -> Void {
+        allItems.append(item)
+    }
+    
+    // Utility Method to Create AND Add New Item to Datastore
+    open static func createAndAddItem() -> Void {
+        allItems.append(Item(random: true))
+    }
+}
